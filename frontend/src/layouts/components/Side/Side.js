bae4ed057a6642f1575131591a25cef4b920a360
inthej <inthej12@gmail.com>
2023-08-01 22:44:44 +0900

    chore: Remove unnecessary CSS attributes

--- frontend/src/layouts/components/Side/Side.js
+++ frontend/src/layouts/components/Side/Side.js
@@ -23,73 +23,73 @@ const Side = (props) => {
           <input className="search" placeholder="Search" type="search" />
         </form>
         <ul className="stack">
-          <li className="row statck selected added">
+          <li className="row selected">
             <span className="title">Title21</span>
           </li>
-          <li className="row statck added">
+          <li className="row">
             <span className="title">Title20</span>
           </li>
-          <li className="row statck added">
+          <li className="row">
             <span className="title">Title19</span>
           </li>
-          <li className="row statck added">
+          <li className="row">
             <span className="title">Title18</span>
           </li>
-          <li className="row statck added">
+          <li className="row">
             <span className="title">Title17</span>
           </li>
-          <li className="row statck added">
+          <li className="row">
             <span className="title">Title16</span>
           </li>
-          <li className="row statck added">
+          <li className="row">
             <span className="title">Title15</span>
           </li>
-          <li className="row statck added">
+          <li className="row">
             <span className="title">Title14</span>
           </li>
-          <li className="row statck added">
+          <li className="row">
             <span className="title">Title13</span>
           </li>
-          <li className="row statck added">
+          <li className="row">
             <span className="title">Title12</span>
           </li>
-          <li className="row statck added">
+          <li className="row">
             <span className="title">Title11</span>
           </li>
-          <li className="row statck added">
+          <li className="row">
             <span className="title">Title10</span>
           </li>
-          <li className="row statck added">
+          <li className="row">
             <span className="title">Title9</span>
           </li>
-          <li className="row statck added">
+          <li className="row">
             <span className="title">Title8</span>
           </li>
-          <li className="row statck added">
+          <li className="row">
             <span className="title">Title7</span>
           </li>
-          <li className="row statck added">
+          <li className="row">
             <span className="title">Title6</span>
           </li>
-          <li className="row statck added">
+          <li className="row">
             <span className="title">Title5</span>
           </li>
-          <li className="row statck added">
+          <li className="row">
             <span className="title">Title4</span>
           </li>
-          <li className="row statck added">
+          <li className="row">
             <span className="title">Title3</span>
           </li>
-          <li className="row statck added">
+          <li className="row">
             <span className="title">Title4</span>
           </li>
-          <li className="row statck added">
+          <li className="row">
             <span className="title">Title3</span>
           </li>
-          <li className="row statck added">
+          <li className="row">
             <span className="title">Title2</span>
           </li>
-          <li className="row statck added">
+          <li className="row">
             <span className="title">Title1</span>
           </li>
         </ul>
